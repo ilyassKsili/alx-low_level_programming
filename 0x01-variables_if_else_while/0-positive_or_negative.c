2f943dc572471f43e44d0cd8177d2574b5c6dfa2
ilyassksili <elksili.ilyass@gmail.com>
2023-07-14 14:41:50 -0700

    tasl 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+  *main - Entry point
+  *Discription: 'check four number if it is positive ou negative'
+  *Return: always 0
+  */
 int main(void)
 {
 	int n;
