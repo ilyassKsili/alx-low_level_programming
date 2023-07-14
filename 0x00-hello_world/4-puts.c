04f00920fec535c88d51696e4ad23eacfc4826e2
ilyassksili <elksili.ilyass@gmail.com>
2023-07-14 05:22:22 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  *main - A program that prints a line with puts fonction
+  *return: 0 (success)
+  */
+int main(void)
+{
+puts("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
