4092adc0f83a4b5ce248399e962b7e2c05674c75
ilyassksili <elksili.ilyass@gmail.com>
2023-07-17 12:03:53 -0700

    task 4& task5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
-  *main - A program that prints a line with puts fonction
-  *return: 0 (success)
+  *main - Entry point
+  *
+  *return: Always 0 (success)
   */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
